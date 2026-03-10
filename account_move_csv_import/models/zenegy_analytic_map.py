6d3e322a5ab5e0f65a136f30d4d6fdfac4b97a35
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2026-03-10 11:34:25 +0100

    Issue69429: Add department code

--- account_move_csv_import/models/zenegy_analytic_map.py
+++ account_move_csv_import/models/zenegy_analytic_map.py
@@ -9,6 +9,7 @@ class ZenegyAnalyticMap(models.Model):
     _name = "zenegy.analytic.map"
     _description = "Zenegy Analytic Map"  # TODO
 
-    name = fields.Char('Department Name', required=True)
+    code = fields.Integer('Department Code', required=True, default=0)
+    name = fields.Char('Department Name')
     analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic account')
     analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic tags')
