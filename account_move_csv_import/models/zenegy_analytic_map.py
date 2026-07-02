599068aceb83766fda9f413ede03f59a899b4a69
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2026-07-02 17:27:50 +0200

    Skip analytic_tag_ids for now

--- account_move_csv_import/models/zenegy_analytic_map.py
+++ account_move_csv_import/models/zenegy_analytic_map.py
@@ -15,7 +15,7 @@ class ZenegyAnalyticMap(models.Model):
     code = fields.Integer('Department Code', required=True, default=0)
     name = fields.Char('Department Name')
     analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic account')
-    analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic tags')
+    # TODO v19: analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic tags')
     repost_crit_acount_ids = fields.Many2many(
         'account.account', string='Repost sum from accounts',
         default=lambda self: self.env['account.account'].search([('code', 'in', ['73350', '73360', '73365', '73370', '73375', '73380'])])
